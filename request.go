ebaf5ba559ca4b67b6e7d45b69d0393b07a06cc1
Louise van der Lith <louisevanderlith@gmail.com>
2020-01-21 17:29:58 +0200

    gitflow-feature-stash: request_pagedata

---
+++ request.go
@@ -0,0 +1,27 @@
+package droxo
+
+import "strconv"
+
+func GetPageData(pageData string) (int, int) {
+	defaultPage := 1
+	defaultSize := 10
+
+	if len(pageData) < 2 {
+		return defaultPage, defaultSize
+	}
+
+	pChar := []rune(pageData[:1])
+
+	if len(pChar) != 1 {
+		return defaultPage, defaultSize
+	}
+
+	page := int(pChar[0]) % 32
+	pageSize, err := strconv.Atoi(pageData[1:])
+
+	if err != nil {
+		return defaultPage, defaultSize
+	}
+
+	return page, pageSize
+}
